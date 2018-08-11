08bbceaa63a56ca5b502b9ce6e1c5f4c187e4817
danish4114 <mohd_danish@excellencetechnologies.in>
2018-08-11 17:21:13 +0530

    for todo

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./App.css";
+import Todo from "./todo";
+import Addtodolist from "./addtodolist";
+import data from "./data.json";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { jsonData: data, value: "", id: 3, newValue: "" };
+  }
+  targetHandler = e => {
+    let clone = this.state.jsonData;
+    clone[e.target.id].completed = !clone[e.target.id].completed;
+    this.setState({ jsonData: clone });
+  };
+  inputTargetHandler = event => {
+    this.setState({ value: event.target.value });
+  };
+  inputSubmission = (event, prevState) => {
+    event.preventDefault();
+    this.setState(prevState => {
+      return { id: prevState.id + 1 };
+    });
+    if (this.state.value === "") {
+      alert("Please Enter Any One Todo Item");
+    } else {
+      var inputData = this.state.value;
+      data.push({ id: this.state.id, completed: true, text: inputData });
+      this.setState({ value: "" });
+    }
+  };
+  editTodo = event => {
+    let dataClone = this.state.jsonData;
+    dataClone[event.target.id].text = event.target.value;
+    if (event.target.value === "") {
+      dataClone.splice(event.target.id, 1);
+      this.setState({ dataClone: dataClone });
+    }
+    this.setState({ jsonData: dataClone });
+  };
+  toDelete = event => {
+    let dataClone = this.state.jsonData;
+    dataClone.splice(event.target.id, 1);
+    this.setState({ dataClone: dataClone });
+  };
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <Todo
+          data={this.state.jsonData}
+          function={e => this.targetHandler(e)}
+          forEdit={event => this.editTodo(event)}
+          toDelete={this.toDelete}
+        />
+        <Addtodolist
+          submit={event => this.inputSubmission(event)}
+          fetch={this.state.value}
+          target={this.inputTargetHandler}
+        />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/todo.js
@@ -0,0 +1,38 @@
+import _ from "lodash";
+import React from "react";
+import AddToDoList from "./addtodolist";
+export default function Todo(props) {
+  const content = props.data;
+  const result = _.map(content, (values, i) => {
+    return (
+      <div className="checkbox" key={values.id}>
+        <div className="list" index={values.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={values.completed}
+              onChange={props.function}
+              id={i}
+            />
+            <input
+              className="forEdit"
+              type="text"
+              value={values.text}
+              id={i}
+              onChange={props.forEdit}
+            />
+          </label>
+          <button className='btn-xsm btn-danger' type="button" id={i} onClick={props.toDelete}>Delete</button>
+          {values.completed && <span className="badge">completed</span>}
+        </div>
+        
+      </div>
+    );
+  });
+  return (
+    <div className="container">
+      <h3>Todo App</h3>
+      {result}
+    </div>
+  );
+}
